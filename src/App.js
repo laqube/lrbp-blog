be64428100786ce63b22088d610c8b3428820f63
Miras <91594617+laqube@users.noreply.github.com>
2023-01-09 22:37:52 +0600

    initial commit
    all setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+// necessary stuff
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+// pages
+import Feed from './pages/feed';
+import Reg from './pages/registration';
+import Log from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Feed/>}/>
+      
+      <Route path="/registration" element={<Reg/>}/>
+      <Route path="/login" element={<Log/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/feed.js
@@ -0,0 +1,22 @@
+// react imports
+import React from 'react';
+
+// components
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+// mui stuff
+
+
+
+function Feed(){
+    return(
+
+        <meat>
+            <h1>Hey there</h1>
+        </meat>
+
+    )
+}
+
+export default Feed;
