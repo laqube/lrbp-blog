5ecf83b5bd702be1a5ce7557da26194be5b51fa1
Miras <91594617+laqube@users.noreply.github.com>
2023-01-10 15:27:30 +0600

    feat | crud ops

--- src/App.js
+++ src/App.js
@@ -7,21 +7,40 @@ import { Routes, Route } from "react-router-dom";
 import Feed from './pages/feed';
 import Reg from './pages/registration';
 import Log from './pages/login';
-
+import CRUDOp from './pages/crudop';
 //components
 import Header from './components/header';
 import Footer from './components/footer';
 
+//mui
+import Stack from '@mui/material/Stack';
+
 function App() {
   return (
-    <><><Header></Header><Routes>
+    <Stack
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+      spacing={5}
+    >
+      <Header/>
+      <Routes>
 
-      <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed />} />
 
-      <Route path="/registration" element={<Reg />} />
-      <Route path="/login" element={<Log />} />
+        <Route path="/registration" element={<Reg />} />
+        <Route path="/login" element={<Log />} />
+        <Route path="/crud" element={<CRUDOp/>} />
 
-    </Routes></><Footer alignItems="flex-end"></Footer></>
+      </Routes>
+      <Footer alignItems="flex-end"
+              sx={{marginTop: 'calc(10% + 60px)',
+              width: '100%',
+              position: 'fixed',
+              bottom: 0,
+              width: '100%'
+              }}/>
+    </Stack>
     
   );
 }
