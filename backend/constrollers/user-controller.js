bddc0bb4a01ecb3c8d42d7b55628376dd1b13840
Miras <91594617+laqube@users.noreply.github.com>
2023-01-11 06:12:48 +0600

    feat | password encryption
    -> implemented bcrypt library so now passwords are safe
    -> next I will work on JWT tokens

--- backend/constrollers/user-controller.js
+++ backend/constrollers/user-controller.js
@@ -1,11 +1,26 @@
 const User = require('../model/User');
+const bcrypt = require("bcryptjs");
+
 
 const registration = async(req, res, next) =>{
+    const{username, email, role, password} = req.body;
+    let existingUser;
+    try{
+        existingUser = await User.findOne({email: email});
+    }catch(err){
+        console.log(err);    
+    }
+    if(existingUser){
+        return res
+        .status(400)
+        .json({message: "Email is already used >:( Try another or log in"})
+    }
+    const hashedPassword = bcrypt.hashSync(password);
     const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        role: req.body.role,
-        password: req.body.password
+        username,
+        email,
+        role,
+        password: hashedPassword //now passwords are secure!
     });
 
     try{
@@ -16,4 +31,24 @@ const registration = async(req, res, next) =>{
     return res.status(201).json({message:user})
 }
 
+const login = async (req, res, next) =>{
+    const {email, password} = req.body;
+
+    let existingUser;
+    try{
+        existingUser = await User.findOne({email: email});
+    }catch(err){
+        return new Error(err);
+    }if(!existingUser){
+        return res.status(400).json({message: "Email is not correct >:("})
+
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message: "Password is wrong! >:("})
+    }
+    return res.status(200).json({message: "Molodec, kirdik"})
+}
+
 exports.registration = registration;
+exports.login = login;
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { registration } = require('../constrollers/user-controller');
+const { registration, login } = require('../constrollers/user-controller');
 
 const router = express.Router();
 
 router.post("/registration", registration);
+router.post("/login", login );
 module.exports = router;
